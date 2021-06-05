a2708b1b3dc17980e2f0c2105e5cf204c4de3d35
Theofilusarifin <theofilusarifin@gmail.com>
2021-06-05 10:54:32 +0700

    Update Class

---
+++ Theofilus Arifin_Timotius Ivan_FinalBattleGame/Theofilus Arifin_Timotius Ivan_FinalBattleGame/GameActor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Theofilus_Arifin_Timotius_Ivan_FinalBattleGame
+{
+    public class GameActor
+    {
+    }
+}
