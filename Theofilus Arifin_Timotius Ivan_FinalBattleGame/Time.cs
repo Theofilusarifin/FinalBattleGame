75589debc2f68054da3792b84f3f0f4f1230b7fa
TimotiusIvan <fernandoivan900@gmail.com>
2021-06-12 22:33:26 +0700

    Update Class Time
    Add Properties and Method

--- Theofilus Arifin_Timotius Ivan_FinalBattleGame/Time.cs
+++ Theofilus Arifin_Timotius Ivan_FinalBattleGame/Time.cs
@@ -23,21 +23,73 @@ namespace Theofilus_Arifin_Timotius_Ivan_FinalBattleGame
         #region PROPERTIES
         public int Hour 
         { 
-            get => hour; 
-            set => hour = value; 
+            get => hour;
+            set
+            {
+                if (value >= 0 && value <= 23)
+                {
+                    hour = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Please enter the number in range correctly");
+                }
+            } 
         }
         public int Minute 
         { 
-            get => minute; 
-            set => minute = value; 
+            get => minute;
+            set
+            {
+                if (value >= 0 && value <= 59)
+                {
+                    minute = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Please enter the number in range correctly");
+                }
+            } 
         }
         public int Second 
         { 
-            get => second; 
-            set => second = value; 
+            get => second;
+            set
+            {
+                if (value >= 0 && value <= 59)
+                {
+                    second = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Please enter the number in range correctly");
+                }
+            } 
         }
         #endregion
         #region METHODS
+        public int ConvertToSecond()
+        {
+            int totalSecond = Second + Minute * 60 + Hour * 3600;
+            return totalSecond;
+        }
+        public void AddSecond(int addSecond)
+        {
+            int currentTotalSecond = ConvertToSecond();
+
+            int totalSecond = currentTotalSecond + addSecond;
+
+            Hour = totalSecond / 3600;
+            Minute = totalSecond % 3600 / 60;
+            Second = totalSecond % 3600 % 60;
+        }
+        public string DisplayTime()
+        {
+            string time = Hour.ToString().PadLeft(2, '0') + ":" +
+                          Minute.ToString().PadLeft(2, '0') + ":" +
+                          Second.ToString().PadLeft(2, '0');
+            return time;
+        }
         #endregion
     }
 }
